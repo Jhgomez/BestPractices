1985fb7f4e29ea46b988689ef0636248bf63b13c
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-08 22:59:46 -0600

    chore: configure network module

---
+++ data/network/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.secrets)
+}
+
+android {
+    namespace = "com.demo.data.network"
+    compileSdk {
+        version = release(37) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    flavorDimensions += "httpclient"
+
+    productFlavors {
+        create("retrofit") {
+            dimension = "httpclient"
+        }
+
+        create("okhttp") {
+            dimension = "httpclient"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
