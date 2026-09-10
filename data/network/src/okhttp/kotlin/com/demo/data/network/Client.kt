17b0cea2d63b0086dfe134cf039924cffa55eb97
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-10 02:45:22 -0600

    chore: add interceptors file

--- data/network/src/okhttp/kotlin/com/demo/data/network/Client.kt
+++ data/network/src/okhttp/kotlin/com/demo/data/network/Client.kt
@@ -2,17 +2,48 @@ package com.demo.data.network
 
 import android.content.Context
 import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import java.io.File
 
-var appContext: Context? = null
-
-val client =  OkHttpClient.Builder()
-    .cache(
-        Cache(
-            directory = File(appContext?.cacheDir, "http_cache"),
-            // $0.05 worth of phone storage in 2020
-            maxSize = 50L * 1024L * 1024L // 50 MiB
-        )
-    )
+val client =  OkHttpClient
+    .Builder()
+    .addInterceptor(HttpLoggingInterceptor())
+    .addInterceptor(BaseUrlInterceptor())
+    .addInterceptor(AuthInterceptor())
     .build()
+class BaseUrlInterceptor: Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        client.newCall(
+            
+        )
+
+        val request = originalRequest
+            .newBuilder()
+            .url(BuildConfig.BASE_URL + originalRequest.url)
+            .build()
+
+        return chain.proceed(request)
+    }
+}
+
+class AuthInterceptor: Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        val request = originalRequest
+            .newBuilder()
+            .header("Authorization: Bearer", BuildConfig.ACCESS_TOKEN)
+            .method(originalRequest.method, originalRequest.body)
+            .build()
+
+        return chain.proceed(request)
+    }
+
+}
