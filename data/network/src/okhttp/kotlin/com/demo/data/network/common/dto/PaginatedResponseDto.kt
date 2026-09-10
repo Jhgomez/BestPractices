9ecbe668401eef9d363dfc2882b61efee1c329a6
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-10 01:54:25 -0600

    refactor: move dtos to proper packages

---
+++ data/network/src/okhttp/kotlin/com/demo/data/network/common/dto/PaginatedResponseDto.kt
@@ -0,0 +1,14 @@
+package com.demo.data.network.common.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PaginatedResponseDto<T>(
+    val page: Int,
+    val results: List<T>,
+    @SerialName("total_pages")
+    val totalPages: Int,
+    @SerialName("total_results")
+    val totalResults: Int,
+)
