89972f6f83b999ce2e9e34cdef98e0af5d0a4fe8
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-10 19:38:06 -0600

    chore: add tvshow dto

---
+++ data/api/tvshow/src/main/kotlin/com/demo/data/api/tvshow/dto/TvShowDto.kt
@@ -0,0 +1,33 @@
+package com.demo.data.network.tvshow.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TvShowDto(
+    val adult: Boolean,
+    @SerialName("backdrop_path")
+    val backdropPath: String?,
+    val id: Int,
+    val name: String,
+    @SerialName("original_language")
+    val originalLanguage: String,
+    @SerialName("original_name")
+    val originalName: String,
+    val overview: String,
+    @SerialName("poster_path")
+    val posterPath: String?,
+    @SerialName("media_type")
+    val mediaType: String,
+    @SerialName("genre_ids")
+    val genreIds: List<Int>,
+    val popularity: Double,
+    @SerialName("first_air_date")
+    val firstAirDate: String,
+    @SerialName("vote_average")
+    val voteAverage: Double,
+    @SerialName("vote_count")
+    val voteCount: Int,
+    @SerialName("origin_country")
+    val originCountry: List<String>
+)
