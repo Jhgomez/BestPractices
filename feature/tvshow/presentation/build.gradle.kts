2ccfbc576c3de801ba4815e521e5651c6956078d
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-11 00:30:18 -0600

    chore: add tvshow empty presentation files

--- feature/tvshow/presentation/build.gradle.kts
+++ feature/tvshow/presentation/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.viewmodel)
 }
---
+++ feature/tvshow/presentation/src/main/kotlin/com/demo/feature/tvshow/presentation/TvShowViewModel.kt
@@ -0,0 +1,7 @@
+package com.demo.feature.tvshow.presentation
+
+import com.demo.feature.tvshow.domain.TvShowRepo
+
+class TvShowViewModel(private val repo: TvShowRepo) {
+
+}
