6a9eb6dda4f6e8b6e758ee3d024524416a62087e
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-10 19:44:52 -0600

    chore: configure domain tvshow android library

---
+++ domain/tvshow/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.demo.data.domain.tvshow"
+    compileSdk {
+        version = release(37) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+//            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    flavorDimensions += listOf("httpclient", "di")
+
+    productFlavors {
+        create("retrofit") {
+            dimension = "httpclient"
+        }
+
+        create("okhttp") {
+            dimension = "httpclient"
+        }
+
+        create("dagger") {
+            dimension = "di"
+        }
+
+        create("hilt") {
+            dimension = "di"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
