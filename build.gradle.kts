5d31accca6751c3053595c15831dadad2bc790ed
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-10 18:59:08 -0600

    chore: add kotlin plugin to classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ktx.serialization) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
