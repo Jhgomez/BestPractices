2ce40c3028f27d4fd50e00e609b74bdd2d16fe2f
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-11 00:10:34 -0600

    chore: add dependency to feature's domain

--- feature/tvshow/presentation/build.gradle.kts
+++ feature/tvshow/presentation/build.gradle.kts
@@ -53,10 +53,11 @@ android {
 }
 
 dependencies {
-      implementation(platform(libs.androidx.compose.bom))
-      implementation(libs.androidx.compose.ui)
-      implementation(libs.androidx.compose.ui.graphics)
-      implementation(libs.androidx.compose.ui.tooling)
-      implementation(libs.androidx.compose.ui.tooling.preview)
-      implementation(libs.androidx.compose.material3)
+    implementation(project(":feature:tvshow:domain"))
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 }
