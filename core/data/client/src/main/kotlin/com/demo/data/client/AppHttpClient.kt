9689c8f2c52dd2e45b47f31d13860cc5108137bd
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-11 10:37:06 -0600

    chore: add proppert default implementations

--- core/data/client/src/main/kotlin/com/demo/data/client/AppHttpClient.kt
+++ core/data/client/src/main/kotlin/com/demo/data/client/AppHttpClient.kt
@@ -7,14 +7,18 @@ interface AppHttpClient {
      * We use an array of Pairs since putting them in a Map is an overkill just to pass parameters
      * it would waste to many resources without any purpose, an Array is much cheaper here`
      */
-    suspend fun <T> get(
+    suspend fun <T: Any> get(
         path: String,
         kType: KType,
         vararg params: Pair<String, String>
-    ): T? = null
+    ): T {
+        throw UnsupportedOperationException()
+    }
 
     /**
      * This function only purpose is to hide retrofit behind our own http client interface
      */
-    fun <T> createService(serviceClass: Class<T>): T? = null
+    fun <T: Any> createService(serviceClass: Class<T>): T {
+        throw UnsupportedOperationException()
+    }
 }
