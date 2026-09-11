0af8b247521e3b47937c5b18a5af2c907f61a07a
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-11 13:55:07 -0600

    chore: add retrofit kotlinx serialization converter

--- core/data/client/src/retrofit/kotlin/com/demo/core/data/client/RetrofitClientImpl.kt
+++ core/data/client/src/retrofit/kotlin/com/demo/core/data/client/RetrofitClientImpl.kt
@@ -2,11 +2,14 @@ package com.demo.core.data.client
 
 import com.demo.data.client.AuthInterceptor
 import com.demo.data.client.BuildConfig
+import kotlinx.serialization.json.Json
 import okhttp3.ConnectionPool
 import okhttp3.Dispatcher
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import java.time.Duration
 import java.util.concurrent.TimeUnit
 
@@ -20,6 +23,16 @@ val connectionPool = ConnectionPool(
     keepAliveDuration = 60,
     timeUnit = TimeUnit.SECONDS
 )
+
+val json = Json {
+    explicitNulls = false
+    prettyPrint = true
+    ignoreUnknownKeys = true
+    coerceInputValues = true
+    allowTrailingComma = true
+    allowComments = true
+    isLenient = true
+}
 
 // retrofit includes okhttp as a transient dependency(built on top of okhttp)
 val client =  OkHttpClient
@@ -36,4 +49,7 @@ val retrofit = Retrofit
     .Builder()
     .baseUrl(BuildConfig.BASE_URL)
     .client(client)
+    .addConverterFactory(
+        json.asConverterFactory("application/json; charset=utf-8".toMediaType())
+    )
     .build()
