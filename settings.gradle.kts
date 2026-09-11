c08e4e28e400ddf74decc590ee47099e16367921
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-11 14:01:36 -0600

    refactor: flatten directories of api common module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ rootProject.name = "BestPractices"
 include(":app")
 
 include(":core:data:client")
-include(":core:data:api:common")
+include(":core:data:api-common")
 include(":core:domain:common")
 
 include(":feature:tvshow:data")
