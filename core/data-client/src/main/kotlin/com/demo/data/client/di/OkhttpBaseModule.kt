a0efd862497bd3b1b3c0f3162c6dc1931a127422
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-12 19:44:17 -0600

    chore: add okhttp base module

---
+++ core/data-client/src/main/kotlin/com/demo/data/client/di/OkhttpBaseModule.kt
@@ -0,0 +1,37 @@
+package com.demo.data.client.di
+
+import com.demo.data.client.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import okhttp3.ConnectionPool
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient.Builder
+import okhttp3.logging.HttpLoggingInterceptor
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+
+@Module
+interface OkhttpBaseModule {
+
+    @Provides
+    fun providesOkhttpConfiguredBaseBuilder(): Builder {
+        val dispatcher = Dispatcher().apply {
+            maxRequestsPerHost = 8
+            maxRequests = 16
+        }
+
+        val connectionPool = ConnectionPool(
+            maxIdleConnections = 16,
+            keepAliveDuration = 60,
+            timeUnit = TimeUnit.SECONDS
+        )
+
+        return Builder()
+            .addInterceptor(HttpLoggingInterceptor())
+            .addInterceptor(AuthInterceptor())
+            .dispatcher(dispatcher)
+            .connectionPool(connectionPool)
+            .connectTimeout(Duration.ofSeconds(6))
+            .callTimeout(Duration.ofSeconds(16))
+    }
+}
