180649230db3f69c4b07de2fcb7d95439d49784a
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-13 10:10:28 -0600

    bugfix: use correct import

--- feature/tvshow/data-api/src/okhttp/kotlin/com/demo/feature/tvshow/data/api/TvShowServiceImpl.kt
+++ feature/tvshow/data-api/src/okhttp/kotlin/com/demo/feature/tvshow/data/api/TvShowServiceImpl.kt
@@ -2,7 +2,7 @@ package com.demo.feature.tvshow.data.api
 
 import com.demo.core.data.client.get
 import com.demo.core.data.model.common.PaginatedResponseDto
-import com.demo.feature.tvshow.data.dto.TvShowDto
+import com.demo.feature.tvshow.data.model.TvShowDto
 import okhttp3.OkHttpClient
 import kotlin.reflect.typeOf
 
