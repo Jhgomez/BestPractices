61b63eef10650e57454b465a8e0242dccadca46c
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-14 00:41:36 -0600

    chore: include tvshow data repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,6 +32,7 @@ include(":core:domain:common")
 
 include(":feature:tvshow:data-api")
 include(":feature:tvshow:data-model")
+include(":feature:tvshow:data-repository")
 include(":feature:tvshow:domain")
 include(":feature:tvshow:presentation")
 
