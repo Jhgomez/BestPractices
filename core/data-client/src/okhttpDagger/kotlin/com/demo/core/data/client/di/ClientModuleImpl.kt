7d530c11c4d017c4c53fbef087e6f5392c28aef5
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-13 22:55:29 -0600

    chore: implement okhttp client module

--- core/data-client/src/okhttpDagger/kotlin/com/demo/core/data/client/di/ClientModuleImpl.kt
+++ core/data-client/src/okhttpDagger/kotlin/com/demo/core/data/client/di/ClientModuleImpl.kt
@@ -1,19 +1,19 @@
 package com.demo.core.data.client.di
 
 import com.demo.core.data.client.BaseUrlInterceptor
+import com.demo.data.client.di.ClientModule
 import com.demo.data.client.di.OkhttpBaseModule
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import javax.inject.Singleton
 
-interface yo
 @Module(includes = [OkhttpBaseModule::class])
-object ClientModule {
+object ClientModuleImpl: ClientModule<OkHttpClient> {
 
     @Singleton
     @Provides
-    fun provideClient(builder: OkHttpClient.Builder): OkHttpClient =
+    override fun provideClient(builder: OkHttpClient.Builder): OkHttpClient =
         builder
             .addInterceptor(BaseUrlInterceptor())
             .build()
