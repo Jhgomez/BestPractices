a4185644abab61116e4ac449de99dc80cf9a04fe
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-13 23:40:09 -0600

    chore: declare app component

---
+++ app/src/dagger/kotlin/di/ApplicationComponent.kt
@@ -0,0 +1,14 @@
+package di
+
+import com.demo.MainActivity
+import com.demo.core.data.client.di.ClientModuleImpl
+import com.demo.feature.tvshow.data.api.di.FeatureTvShowDataModuleImpl
+import dagger.Component
+
+@Component(modules = [
+    ClientModuleImpl::class,
+    FeatureTvShowDataModuleImpl::class
+])
+interface ApplicationComponent {
+    fun inject(activity: MainActivity)
+}
