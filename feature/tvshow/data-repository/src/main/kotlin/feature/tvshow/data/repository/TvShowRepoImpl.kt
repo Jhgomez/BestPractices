92babafa4e4f50b33fdfa657cffb64bc8e80a134
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-14 01:58:45 -0600

    chore: add impl of repo

---
+++ feature/tvshow/data-repository/src/main/kotlin/feature/tvshow/data/repository/TvShowRepoImpl.kt
@@ -0,0 +1,37 @@
+package feature.tvshow.data.repository
+
+import com.demo.core.domain.common.model.Page
+import com.demo.feature.tvshow.data.api.TvShowService
+import com.demo.feature.tvshow.domain.TvShow
+import com.demo.feature.tvshow.domain.TvShowRepo
+
+class TvShowRepoImpl(private val service: TvShowService): TvShowRepo {
+    override suspend fun getTvShow(page: Int): Page<TvShow> {
+        val response = service.getTvShows(page)
+
+        return Page(
+            page = response.page,
+            results = response.results.map { dto ->
+                TvShow(
+                    adult = dto.adult,
+                    backdropPath = dto.backdropPath,
+                    id = dto.id,
+                    name = dto.name,
+                    originalLanguage = dto.originalLanguage,
+                    originalName = dto.originalName,
+                    overview = dto.overview,
+                    posterPath = dto.posterPath,
+                    mediaType = dto.mediaType,
+                    genreIds = dto.genreIds,
+                    popularity = dto.popularity,
+                    firstAirDate = dto.firstAirDate,
+                    voteAverage = dto.voteAverage,
+                    voteCount = dto.voteCount,
+                    originCountry = dto.originCountry
+                )
+            },
+            totalPages = response.totalPages,
+            totalResults = response.totalResults
+        )
+    }
+}
---
+++ feature/tvshow/data-repository/src/main/kotlin/feature/tvshow/data/repository/di/TvShowRepoModule.kt
@@ -0,0 +1,7 @@
+package feature.tvshow.data.repository.di
+
+import com.demo.feature.tvshow.domain.TvShowRepo
+
+interface TvShowRepoModule {
+//    fun bindTvShowRepository(): TvShowRepo
+}
