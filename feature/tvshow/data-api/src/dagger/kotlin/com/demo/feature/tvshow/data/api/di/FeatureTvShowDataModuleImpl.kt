a70936ff5822c86d9fc81b10e5c55d245f37ae64
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-13 23:29:54 -0600

    chore: add common dagger feature tv show data module

---
+++ feature/tvshow/data-api/src/dagger/kotlin/com/demo/feature/tvshow/data/api/di/FeatureTvShowDataModuleImpl.kt
@@ -0,0 +1,16 @@
+package com.demo.feature.tvshow.data.api.di
+
+import com.demo.feature.tvshow.data.api.TvShowService
+import com.demo.feature.tvshow.data.api.TvShowServiceImpl
+import com.demo.feature.tvshow.data.di.FeatureTvShowDataModule
+import dagger.Binds
+import dagger.Module
+import javax.inject.Singleton
+
+@Module
+interface FeatureTvShowDataModuleImpl : FeatureTvShowDataModule {
+
+    @Binds
+    @Singleton
+    override fun bindsService(service: TvShowServiceImpl): TvShowService
+}
