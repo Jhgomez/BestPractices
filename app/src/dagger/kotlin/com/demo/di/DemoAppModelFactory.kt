33cc611e4c67f4d98975fc761ea1418ac786d6cb
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-15 00:06:30 -0600

    chore: add vm factory

---
+++ app/src/dagger/kotlin/com/demo/di/DemoAppModelFactory.kt
@@ -0,0 +1,58 @@
+package com.demo.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.demo.MyViewModel
+import dagger.Binds
+import dagger.MapKey
+import dagger.Module
+import dagger.multibindings.IntoMap
+import javax.inject.Inject
+import javax.inject.Provider
+import kotlin.reflect.KClass
+
+class DemoAppModelFactory @Inject constructor(
+    private val creators: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        var creator: Provider<out ViewModel>? = creators[modelClass]
+        if (creator == null) {
+            for ((key, value) in creators) {
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+        if (creator == null) {
+            throw IllegalArgumentException("Unknown model class: $modelClass")
+        }
+        try {
+            @Suppress("UNCHECKED_CAST")
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
+
+@Module
+abstract class ViewModelBuilderModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(
+        factory: DemoAppModelFactory
+    ): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MyViewModel::class)
+    abstract fun bindViewModel(viewmodel: MyViewModel): ViewModel
+}
+
+@Target(
+    AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
