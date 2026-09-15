7a561c4cfc5e05b60ee062828641b09a895befc6
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-15 17:36:56 -0600

    chore: inject repo into tvshow vm

--- feature/tvshow/presentation/src/main/kotlin/com/demo/feature/tvshow/presentation/TvShowViewModel.kt
+++ feature/tvshow/presentation/src/main/kotlin/com/demo/feature/tvshow/presentation/TvShowViewModel.kt
@@ -1,9 +1,19 @@
 package com.demo.feature.tvshow.presentation
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.demo.feature.tvshow.domain.TvShow
 import com.demo.feature.tvshow.domain.TvShowRepo
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class TvShowViewModel (): ViewModel() {
+internal class TvShowViewModel @Inject constructor(private val repo: TvShowRepo): ViewModel() {
+    val movies = mutableStateOf(emptyList<TvShow>())
 
+    fun getTvShows() {
+        viewModelScope.launch {
+            movies.value = repo.getTvShow(1).results
+        }
+    }
 }
