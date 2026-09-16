6e78df160a0704a82904188ce7c533f815bf5e63
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-16 11:45:53 -0600

    chore: add navigation 3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.androidx.material3.adaptive.navigation3)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
