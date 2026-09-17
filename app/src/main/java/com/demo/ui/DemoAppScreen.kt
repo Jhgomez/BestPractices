8714122166a4f4a2c18bd585531d4ef2254b6e56
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-16 19:35:39 -0600

    chore: add demo screen

---
+++ app/src/main/java/com/demo/ui/DemoAppScreen.kt
@@ -0,0 +1,12 @@
+package com.demo.ui
+
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DemoAppScreen(
+    modifier: Modifier,
+) {
+
+}
