613cf241def5337ee6ae90a2cc644adfe81ff61e
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-18 02:18:47 -0600

    chore: configure navigation suite scaffold items

--- app/src/main/java/com/demo/ui/DemoAppScreen.kt
+++ app/src/main/java/com/demo/ui/DemoAppScreen.kt
@@ -1,34 +1,49 @@
 package com.demo.ui
 
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.intl.LocaleList
-import com.demo.ui.navigation.TopLevelKeys
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation3.runtime.NavKey
+import com.demo.R
+import com.demo.ui.navigation.TopLevelDestination
 
 @Composable
 fun DemoAppScreen(
-    modifier: Modifier,
+    onNavigate: (NavKey) -> Unit,
+    modifier: Modifier
 ) {
     // This composable saves us the need to configure a screen strategy for showing navigation UI
     // but only the nav bottom bar and nav rail, a top bar is not included, and since I want a top
     // bar in some screens I will have to create one but it will be a single top bar for the whole app
     NavigationSuiteScaffold(
         navigationSuiteItems = {
-            TopLevelKeys.entries.forEach {
+            TopLevelDestination.entries.forEach { destination ->
                 item(
                     selected = false,
                     onClick = {
 
                     },
-                    icon = {},
+                    icon = {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(id = destination.icon),
+                            contentDescription = null
+                        )
+                    },
                     modifier = Modifier,
                     enabled = true,
-                    label = {},
+                    label = {
+                        Text(text = stringResource(id = destination.title))
+                    },
                     alwaysShowLabel = true,
-                    badge = {},
+                    badge = {
+                        // this is used to display thinks like unread messages
+                    },
                     colors = null,
                     interactionSource = null
                 )
