f631cb5ff40ded3654a9c6baa2728ae8de4f61e6
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-17 20:27:12 -0600

    chore: add navigation suite scaffold skeleton

--- app/src/main/java/com/demo/ui/DemoAppScreen.kt
+++ app/src/main/java/com/demo/ui/DemoAppScreen.kt
@@ -1,12 +1,41 @@
 package com.demo.ui
 
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteItem
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.intl.LocaleList
+import com.demo.ui.navigation.TopLevelKeys
 
 @Composable
 fun DemoAppScreen(
     modifier: Modifier,
 ) {
+    // This composable saves us the need to configure a screen strategy for showing navigation UI
+    // but only the nav bottom bar and nav rail, a top bar is not included, and since I want a top
+    // bar in some screens I will have to create one but it will be a single top bar for the whole app
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            TopLevelKeys.entries.forEach {
+                item(
+                    selected = false,
+                    onClick = {
 
+                    },
+                    icon = {},
+                    modifier = Modifier,
+                    enabled = true,
+                    label = {},
+                    alwaysShowLabel = true,
+                    badge = {},
+                    colors = null,
+                    interactionSource = null
+                )
+            }
+        },
+        modifier = Modifier
+    ) {
+
+    }
 }
